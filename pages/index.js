d32d91fdb969896d01ed50f73674f2aaf122b5ca
Вячеслав Коханный <xoxluwa1@yandex.com>
2020-11-24 12:53:59 +0500

    feature: сверстать адаптивно блок intro

--- pages/index.js
+++ pages/index.js
@@ -4,3 +4,23 @@ import { mobileMenuSelectors } from '../utils/constants.js';
 const popupMobileMenu = new PopupMobileMenu(mobileMenuSelectors);
 
 popupMobileMenu.setEventListeners();
+
+
+
+
+
+
+// element.mouseover(function(){
+
+//   // the animation starts
+//   element.toggleClass('hinge animated');
+
+//   // do something when animation ends
+//   element.addEventListener('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', (e) => {
+
+//    // trick to execute the animation again
+//     $(e.target).classList.remove('hinge animated');
+
+//   });
+
+// });
