08a98c2be4603318935cf831102df9b658721dc2
Вячеслав Коханный <xoxluwa1@yandex.com>
2020-11-26 15:37:28 +0500

    feature: сверстать в адаптиве футер

--- pages/index.js
+++ pages/index.js
@@ -2,5 +2,8 @@ import PopupMobileMenu from '../components/PopupMobileMenu.js';
 import { mobileMenuSelectors } from '../utils/constants.js';
 
 const popupMobileMenu = new PopupMobileMenu(mobileMenuSelectors);
+const date = document.querySelector('.js-date');
+
+date.textContent = `© ${new Date().getFullYear()} Все права защищены`;
 
 popupMobileMenu.setEventListeners();
